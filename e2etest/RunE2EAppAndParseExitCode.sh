51d9655d8835d717a75807befa7e716678d1fb8c
Pragna Gopa <pragna.gopa@hotmail.com>
2016-05-03 11:54:36 -0700

    Capture standard output and error

--- e2etest/RunE2EAppAndParseExitCode.sh
+++ e2etest/RunE2EAppAndParseExitCode.sh
@@ -1,18 +1,17 @@
-RunE2EAppOutput=$(./RunE2EApp.sh $1 $2 $3 $4 $5 2>&1 >/dev/null)
+RunE2EAppOutput=$(./RunE2EApp.sh $1 $2 $3 $4 $5 2> errFile)
+ERR=$(<errFile)
 
 echo $RunE2EAppOutput
+echo $ERR
 
 if [[ "$RunE2EAppOutput" == *"disengage"* ]]; then
-echo "fail";
 exit 1
 fi
 
 if [[ "$RunE2EAppOutput" == *"exit 1"* ]]; then
 echo "fail exit";
-exit 1
 fi
 
 if [[ "$RunE2EAppOutput" == *"Script threw an uncaught"* ]]; then
-echo "fail";
 exit 1
 fi
