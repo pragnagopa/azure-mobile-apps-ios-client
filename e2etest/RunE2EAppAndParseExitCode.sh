5a7a3805853804fc05914e05edbfd3a3af203e75
Pragna Gopa <pragna.gopa@hotmail.com>
2016-05-09 15:34:25 -0700

    Update exit

--- e2etest/RunE2EAppAndParseExitCode.sh
+++ e2etest/RunE2EAppAndParseExitCode.sh
@@ -5,13 +5,16 @@ echo $RunE2EAppOutput
 echo $ERR
 
 if [[ "$RunE2EAppOutput" == *"disengage"* ]]; then
+echo disengage
 exit 1
 fi
 
-if [[ "$RunE2EAppOutput" != *"exit 0"* ]]; then
+if [[ "$RunE2EAppOutput" == *"Specified target process is invalid"* ]]; then
+echo Specified target process is invalid
 exit 1
 fi
 
 if [[ "$RunE2EAppOutput" == *"Script threw an uncaught"* ]]; then
+echo Script threw an uncaught
 exit 1
 fi
